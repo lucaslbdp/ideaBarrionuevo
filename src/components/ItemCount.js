402744ad8202e0e43e6e2f61ca4ed2a17f4be50b
Lucas Barrionuevo <lukitaz_lbdp@hotmail.com>
2021-09-30 18:16:55 -0300

    promesa agregada

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 const ItemCount = () => {
     const [counter, setCounter] = useState(1);
     const stock = 10;
-
+    
     const sumar = () => {
         if (counter !== stock) {
             setCounter(counter + 1)
@@ -19,7 +19,7 @@ const ItemCount = () => {
 
     return (
         <div className="container">
-            <h3>Jean {counter}</h3>
+            <h3>Jean {counter}</h3
             <Button className="btn btn-danger" onClick={() => restar()}>-</Button>
             <Button className="btn btn-primary" onClick={() => sumar()}>+</Button>
         </div>
